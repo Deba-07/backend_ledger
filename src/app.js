0a2b6cfe2de54acb4cb548865189bd3fb2f8ca99
Deba-07 <dynamicdeba07@gmail.com>
2026-05-11 19:54:40 +0530

    refactor(app): reorganize imports and add account and transaction routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const cookieParser =  require('cookie-parser')
-const authRouter = require("./routes/auth.routes")
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
-const app = express()
+// Import Routes
+const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
+const transactionRouter = require("./routes/transaction.routes");
 
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
 
-app.use("/api/auth", authRouter)
+app.use(express.json());
+app.use(cookieParser());
 
-module.exports = app
+app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+app.use("/api/transactions", transactionRouter);
+
+module.exports = app;
