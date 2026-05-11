4a17bb70930434899a6a27188bf85a2315567605
Deba-07 <dynamicdeba07@gmail.com>
2026-05-11 20:01:50 +0530

    feat(auth): add logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const authCotroller = require("../controllers/auth.controller")
+const express = require("express");
+const authCotroller = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", authCotroller.userRegisterController)
-router.post("/login", authCotroller.userLoginController)
+router.post("/register", authCotroller.userRegisterController);
+router.post("/login", authCotroller.userLoginController);
+router.post(
+  "/logout",
+  authMiddleware.authMiddleware,
+  authCotroller.userLogoutController,
+);
 
-module.exports = router
+module.exports = router;
